3462142e1abf55da5e6d22017707d04c82856691
wachka06 <wachka06@gmail.com>
2018-07-09 20:55:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,8 @@ katz_deli = []
 
 def line
   if katz_deli.size == 0 
-    puts "The line is currently empty."
+    return "The line is currently empty."
   else 
-    puts katz_deli.size
+    return katz_deli.size
   end
 end
