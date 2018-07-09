7caf5608042abd4f2d835bb28bdeda5d4e475e44
wachka06 <wachka06@gmail.com>
2018-07-09 21:01:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,9 @@ def line(katz_deli)
   if katz_deli.size == 0 
     puts "The line is currently empty."
   else 
-    puts 
+    phrase = "The line is currently: "
+    katz_deli.each_with_index do |name, index|
+      phrase += "#{index}. #{name} "
   end
+    puts phrase
 end
